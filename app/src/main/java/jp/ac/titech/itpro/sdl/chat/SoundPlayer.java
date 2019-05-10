6c013f55259749959ad2d059cf825ed50ba49ed4
malony_0 <hamuta57@yahoo.co.jp>
2019-05-10 16:48:04 +0900

    sound

--- app/src/main/java/jp/ac/titech/itpro/sdl/chat/SoundPlayer.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/chat/SoundPlayer.java
@@ -8,6 +8,7 @@ class SoundPlayer {
     private final SoundPool soundPool;
     private final int soundConnected;
     private final int soundDisconnected;
+    private final int soundPressedButton;
 
     SoundPlayer(Context context) {
         AudioAttributes attrs = new AudioAttributes.Builder()
@@ -24,6 +25,8 @@ class SoundPlayer {
         soundConnected = soundPool.load(context, R.raw.nhk_doorbell, 1);
         // https://www2.nhk.or.jp/archives/creative/material/view.cgi?m=D0002070102_00000
         soundDisconnected = soundPool.load(context, R.raw.nhk_woodblock2, 1);
+        // https://www2.nhk.or.jp/archives/creative/material/view.cgi?m=D0002011497_00000
+        soundPressedButton = soundPool.load(context, R.raw.nhk_cat, 1);
     }
 
     void playConnected() {
@@ -33,4 +36,9 @@ class SoundPlayer {
     void playDisconnected() {
         soundPool.play(soundDisconnected, 1.0f, 1.0f, 0, 0, 1);
     }
+
+    void playButtonPressed() {
+        soundPool.play(soundPressedButton, 1.0f, 1.0f, 0,0,1);
+    }
+
 }
